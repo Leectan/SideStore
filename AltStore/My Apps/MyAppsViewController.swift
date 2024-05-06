2c3049266eebc01213e2c0a6683c1b6275a5d0af
nythepegasus <nythepegasus84@gmail.com>
2024-05-06 17:57:13 -0400

    Include "Enable JIT" errors in Error Log

--- AltStore/My Apps/MyAppsViewController.swift
+++ AltStore/My Apps/MyAppsViewController.swift
@@ -1352,11 +1352,13 @@ private extension MyAppsViewController
     {
         if #available(iOS 17, *) {
             let toastView = ToastView(error: OperationError.tooNewError)
+            AppManager.shared.log(OperationError.tooNewError, operation: .enableJIT, app: installedApp)
             toastView.show(in: self)
             return
         }
         if !minimuxer.ready() {
             let toastView = ToastView(error: MinimuxerError.NoConnection)
+            AppManager.shared.log(MinimuxerError.NoConnection, operation: .connection, app: installedApp)
             toastView.show(in: self)
             return
         }
